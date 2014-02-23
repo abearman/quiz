c8232aeb0a1048c50ca633fee94ee52d2d5014dc
Amy Bearman <amy@hyperfine.com>
2014-02-23 14:27:41 -0800

    Added new classes: HistoryObject, Message, Question, TopScorer to suppress error messages. Please edit these existing classes instead of creating new ones.

--- src/quiz/User.java
+++ src/quiz/User.java
@@ -1,7 +1,18 @@
 package quiz;
+import java.util.*;
 
 public class User {
-
+	
+	/* Instance variables*/
+	boolean isAdministrator;
+	String loginName;
+	String password;
+	ArrayList<String> friends;
+	ArrayList<HistoryObject> historyList;
+	boolean[] achievements;
+	ArrayList<Message> messages;
+	
+	/* Constructor */
 	public User() {
 		
 	}
