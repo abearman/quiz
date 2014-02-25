979bd8469bccc3ad9192fa25ac40a5b5ea3caa47
martinob <martinob@stanford.edu>
2014-02-24 20:37:37 -0800

    Getitng the best score in ChallengeMessage loops through the user's arraylist of historyobjects. HistoryObject has quiName field now

--- src/quiz/ChallengeMessage.java
+++ src/quiz/ChallengeMessage.java
@@ -1,51 +1,57 @@
 package quiz;
 
+import java.util.ArrayList;
+
 public class ChallengeMessage extends Message {
 	
-	private Quiz quiz;
+	private String quizName;
+	private String quizLink;
 	private String message;
+	private double bestScore;
 	
-	public ChallengeMessage(String fromUser, String toUser, Quiz quiz) {
-		super(fromUser, toUser);
-		this.quiz = quiz;
+	public ChallengeMessage(User fromUser, User toUser, Quiz quiz) {
+		super(fromUser.getLoginName(), toUser.getLoginName());
+		this.quizName = quiz.getQuizName();
+		this.quizLink = quiz.getQuizLink();
+		this.bestScore = challengingUserBestScore(fromUser);
 		message = fromUser + "is challenging you to take the " + quiz.getQuizName() + "quiz!";
 	}
 	
 	public String getQuizName() {
-		return quiz.getQuizName();
+		return quizName;
 	}
 	
 	public String getQuizLink() {
-		return quiz.getQuizLink();
+		return quizLink;
 	}
 	
 	public String getMessage() {
 		return message;
 	}
 	
-	public double challengingUserBestScore() {
-		//is this worth it? Seems too costly.
-		double bestScore = 0;
-		for (HistoryObject hist : quiz.getAllHistories()) {
-			if (hist.getUserName().equals(getFromUser())) {
-				double score = hist.getScore();
-				if (score > bestScore)
-					bestScore = score;
-			}
-		}
-		return bestScore;
-	}
-	
-	public double challengingUserBestScore2() {
-		//Alternative looping through user's histories
-		double bestScore = 0;
-//		for (HistoryObject hist : user.getAllHistories()) {
-//			if (user.getQuizName().equals(getQuizName())) {
+//	public double challengingUserBestScore(ArrayList<HistoryObject> allHistories) {
+//		//is this worth it? Seems too costly.
+//		double bestScore = 0;
+//		for (HistoryObject hist : allHistories) {
+//			if (hist.getUserName().equals(getFromUser())) {
 //				double score = hist.getScore();
 //				if (score > bestScore)
 //					bestScore = score;
 //			}
 //		}
+//		return bestScore;
+//	}
+	
+	public double challengingUserBestScore(User user) {
+		//Alternative looping through user's histories
+		double bestScore = 0;
+		for (HistoryObject hist : user.getHistory()) {
+			if (hist.getQuizName().equals(getQuizName())) {
+				double score = hist.getScore();
+				if (score > bestScore)
+					bestScore = score;
+			}
+		}
 		return bestScore;
 	}
 
