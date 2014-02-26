0b66d1cd316b4a9913d5caddc68a6994e0e7362d
Amy Bearman <amy@hyperfine.com>
2014-02-26 10:44:32 -0800

    Updated AccountManager class to query the database as to whether or not accounts exist, passwords are correct, etc. Got rid of the internal HashMap data structure.

--- src/quiz/AccountManager.java
+++ src/quiz/AccountManager.java
@@ -1,46 +1,90 @@
 package quiz;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
 
 public class AccountManager {
 
 	/* Instance variables */
-	private HashMap<String, String> map; //Maps loginNames to passwordHash
 	private DBConnection conn;
 	private Statement stmt;
 	
 	/* Constructor */
 	public AccountManager(DBConnection conn) {
-		map = new HashMap<String, String>();
 		this.conn = conn;
 		this.stmt = conn.getStatement();
 	}
 	
+	/* Private helper method. Given a byte[] array, produces a hex String,
+	 such as "234a6f". with 2 chars for each byte in the array. */
+	private String hexToString(byte[] bytes) {
+		StringBuffer buff = new StringBuffer();
+		for (int i=0; i<bytes.length; i++) {
+			int val = bytes[i];
+			val = val & 0xff;  // remove higher bits, sign
+			if (val<16) buff.append('0'); // leading 0
+			buff.append(Integer.toString(val, 16));
+		}
+		return buff.toString();
+	}
+	
+	/* Private helper method to convert the user's password from clear text
+	 * to a hash, and to store the hash as an instance variable */
+	private String hashPassword(String password) {
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("SHA"); //Initialize the MessageDigest
+			md.reset();
+			byte[] buffer = password.getBytes(); //Converts password to array of bytes
+			md.update(buffer);
+			byte[] digest = md.digest();
+			return hexToString(digest);
+		} catch (NoSuchAlgorithmException ex) {
+			ex.printStackTrace(); //TODO: How should we handle this exception?
+			return null;
+		}
+	}
+	
 	/* Given an account name, returns a boolean representing whether or not the account already exists */
 	public boolean accountExists(String loginName) {
-		if (map.containsKey(loginName)) {
-			return true;
+		String query = "SELECT * FROM users WHERE loginName = \"" + loginName + "\"";
+		try {
+			ResultSet rs = stmt.executeQuery(query);
+			if (rs.next()) { //If there exists a record with this loginName in the database return true
+				return true;
+			}
+			return false;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
 		}
-		return false;
 	}
 	
 	/* Given an account name and a password String, returns a boolean representing whether or not 
 	 * the password parameter matches the password for the specified account */
 	public boolean isPasswordForAccount(String loginName, String passwordClear) {
-		User user = new User(loginName, passwordClear, conn);
-		String passwordHash = user.getPasswordHash(); //Hash the password they enter to log in
-		if (map.get(loginName).equals(passwordHash)) { //Compares the hash of the password they enter to the hash stored when the account was created
-			return true; //If they match, let them log in to this account
+		String query = "SELECT * FROM users WHERE loginName = \"" + loginName + "\"";
+		try {
+			ResultSet rs = stmt.executeQuery(query);
+			String databasePasswordHash = rs.getString("password"); //Retrieves the stored hash from the Database for this User
+			String hashOfAttemptedPassword = hashPassword(passwordClear);
+			if (hashOfAttemptedPassword.equals(databasePasswordHash)) {
+				return true;
+			}
+			return false;
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		return false;
 	}
 	
 	/* Given an account name and a password, creates a new account */
 	public void createNewAccount(String loginName, String passwordClear) {
-		User user = new User(loginName, passwordClear, conn);
-		String passwordHash = user.getPasswordHash(); //Hashes their password and stores the hashed version
-		map.put(loginName, passwordHash);
+		User user = new User(loginName, passwordClear, conn); //The constructor handles putting the User in the database
 	}
 	
 }
