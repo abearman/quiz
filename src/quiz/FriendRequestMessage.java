e4cbfdafec14ea0e91c8ffb7c6de9d37d6139f4a
Amy Bearman <amy@hyperfine.com>
2014-02-26 17:09:09 -0800

    Added functionality so that the constructor of FriendRequestMessage updates the database, using the DAL

--- src/quiz/FriendRequestMessage.java
+++ src/quiz/FriendRequestMessage.java
@@ -6,27 +6,18 @@ import java.sql.Statement;
 public class FriendRequestMessage extends Message {
 	
 	private String message;
+	private DAL dal;
 
-	public FriendRequestMessage(String fromUser, String toUser, DBConnection con) {
-		super(fromUser, toUser, con);
+	public FriendRequestMessage(String fromUser, String toUser, DAL dal) {
+		super(fromUser, toUser, dal);
 		message = fromUser + " wants to be friends with you! Do you accept the request?";
+		this.dal = dal;
+		this.dal.addMessageForUser(fromUser, toUser, Message.FRIEND_REQUEST_MESSAGE, message, null, -1);
 	}
 	
 	public void acceptRequest(boolean accept) {
-		Statement stmt = getCon().getStatement();
 		if (accept) {
-			//update MySQL database
-			try {
-				String fromUser = getFromUser();
-				String toUser = getToUser();
-				
-				String update = "INSERT INTO friends VALUES(\"" + fromUser + "\",\"" + toUser + "\") , "
-														+ "(\"" + toUser + "\",\"" + fromUser + "\")";
-				stmt.executeUpdate(update);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
+			dal.addFriendPair(getFromUser(), getToUser());
 		}
 	}
 	
