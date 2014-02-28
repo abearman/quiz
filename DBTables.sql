506fd1b54d24c97e677e95c041ff90ab99346fa0
Bhaven Patel <heybhaven@yahoo.com>
2014-02-28 00:51:30 -0800

    implemented FriendRecentActivity

--- DBTables.sql
+++ DBTables.sql
@@ -117,7 +117,8 @@ CREATE TABLE users (
     loginName CHAR(64),
     isAdministrator BOOLEAN,
     password CHAR(64),
-    achievements CHAR(64)
+    achievements CHAR(64),
+    recentActivity CHAR(64)
 );
 
 
