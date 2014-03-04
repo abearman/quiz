ffc911e705ac203fc1f021839e3d043802e8cbbd
Pavitra Rengarajan <pavitra.t.rengarajan@gmail.com>
2014-03-03 23:07:20 -0800

    upon completion update history and top scorers database tables

--- src/quiz/DoneWithQuizServlet.java
+++ src/quiz/DoneWithQuizServlet.java
@@ -1,6 +1,8 @@
 package quiz;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -35,7 +37,9 @@ public class DoneWithQuizServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		DAL dal = (DAL)request.getServletContext().getAttribute("DAL");
 		Quiz quiz = (Quiz)request.getSession().getAttribute("quiz");
+		User user = (User)request.getSession().getAttribute("user");
 		ArrayList<Question> questions = quiz.getQuestions();
 		ArrayList<String> answers = quiz.getAnswers(); 
 		
@@ -54,7 +58,11 @@ public class DoneWithQuizServlet extends HttpServlet {
 		quiz.setNumQuestionsCorrect(numQuestionsCorrect);
 		request.getSession().setAttribute("numQuestionsCorrect", numQuestionsCorrect);
 		
-		//TODO add this to the history and to the top scorers
+		//add this to the history and to the top scorers
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		java.util.Date now = new java.util.Date();
+		dal.addToHistoryListForUser(user.getLoginName(), quiz.getQuizName(), numQuestionsCorrect, elapsedTime, df.format(now), now);
+		quiz.addTopScorer(new TopScorer(user.getLoginName(), numQuestionsCorrect, elapsedTime, dal));
 		
 		RequestDispatcher dispatch = request.getRequestDispatcher("quizResults.jsp");
 		dispatch.forward(request,response);
